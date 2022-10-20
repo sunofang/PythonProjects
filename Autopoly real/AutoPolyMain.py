9e36c6fe083f8e885176020ce3754b90369cc099
sunofang <38206283+sunofang@users.noreply.github.com>
2022-10-20 13:56:56 +0200

    Makes Pyoly smaller to prove a point
    I spite you. Discord user.

--- Autopoly real/AutoPolyMain.py
+++ Autopoly real/AutoPolyMain.py
@@ -1,14 +1,10 @@
 import random
 import time
 import json
-
 #Welcome to the nightmare that is P(Y)OLY
 stoppedit = False
 stoppedyet = 0
-
 PolyLines = json.load(open("lines.json"))
-
-
 while stoppedit == False:
     print(random.choice(PolyLines))
     stoppedyet += 1
